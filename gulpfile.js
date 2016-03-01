69ffda7b59d78fda73e5e94893e83a99b7ec584b
kprenesti <krista.prenesti@gmail.com>
2016-03-01 13:13:55 -0500

    Gulp works

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+// ============ SETTING UP REQUIRES ==========//
+
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    plumber = require('gulp-plumber'),
+    autoprefixer = require('gulp-autoprefixer'),
+    imagemin = require('gulp-imagemin'),
+    browserSync = require('browser-sync').create();
+
+//============= SETTING UP TASKS ==========//
+gulp.task('scripts', function(){
+  gulp.src('js/**/*.js')
+  .pipe()
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+}); //end browserSync
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(plumber())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+    gulp.watch('sass/**/*.scss',['styles']);
+}); //end styles
+
+gulp.task('jpgs', function() {
+    return gulp.src('src/img/*.jpg')
+    .pipe(plumber())
+    .pipe(imagemin({ progressive: true }))
+    .pipe(gulp.dest('images'));
+});
+
+gulp.task('default', ['jpgs', 'styles', 'browser-sync', 'scripts']);
